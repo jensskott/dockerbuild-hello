d659ad98b8070feeb065d4d79c09dbc555440592
Simon Nystrom <simon.nystrom@accedo.tv>
2017-12-08 16:37:42 +0100

    Fixed a few things and added health check to main

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,10 @@ FROM alpine:3.6
 
 ARG binary
 
+ENV EXECUTABLE=/opt/${binary}
+
 COPY build/${binary}-linux-amd64 /opt/${binary}
+
+RUN chmod +x /opt/${binary}
+
+CMD ${EXECUTABLE}
--- main.go
+++ main.go
@@ -2,13 +2,26 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	log "github.com/sirupsen/logrus"
 )
 
 func main() {
+
+	RegisterHealthCheck()
+
 	fmt.Println("hello-world")
 	log.WithFields(log.Fields{
 		"animal": "walrus",
 	}).Info("A walrus appears")
+}
+
+func health(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("ok"))
+}
+
+func RegisterHealthCheck() {
+	http.HandleFunc("/health", health)
+	http.ListenAndServe(":8080", nil)
 }
